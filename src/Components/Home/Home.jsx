c158475957dab4b8bc7397218c73e199d8d85f20
Rabeya Akter <rabeyaakterbdcit@gmail.com>
2024-08-10 04:03:05 -0700

    added form

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "../Home/HomeContent/Banner/Banner";
 import CardContent from "../../Components/Home/HomeContent/CardContent/CardContent";
+import FormDemo from "./HomeContent/FormDemo/FormDemo";
 function Home() {
   return (
     <div>
       <Banner></Banner>
       <CardContent></CardContent>
+      <FormDemo></FormDemo>
     </div>
   );
 }
---
+++ src/Components/Home/HomeContent/FormDemo/FormDemo.jsx
@@ -0,0 +1,155 @@
+import React, { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+import {
+  Button,
+  Cascader,
+  Checkbox,
+  ColorPicker,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Select,
+  Slider,
+  Switch,
+  TreeSelect,
+  Upload,
+} from "antd";
+
+function FormDemo() {
+  const { RangePicker } = DatePicker;
+  const { TextArea } = Input;
+  const normFile = (e) => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e?.fileList;
+  };
+
+  return (
+    <div className="bg-pink-50">
+      <div className="container mx-auto pt-20 pb-20">
+        <div className="text-center">
+          <h1 className="font-bold text-6xl mb-24">
+            Lorem ipsum dolor sit amet <br />
+            consectetur adipisicing.
+          </h1>
+        </div>
+
+        <Form
+          labelCol={{
+            span: 4,
+          }}
+          wrapperCol={{
+            span: 14,
+          }}
+          layout="horizontal"
+          style={{
+            maxWidth: 600,
+          }}
+        >
+          <Form.Item label="Agree?" name="disabled" valuePropName="checked">
+            <Checkbox></Checkbox>
+          </Form.Item>
+          <Form.Item label="Radio">
+            <Radio.Group>
+              <Radio value="apple"> Apple </Radio>
+              <Radio value="pear"> Pear </Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item label="Input">
+            <Input />
+          </Form.Item>
+          <Form.Item label="Select">
+            <Select>
+              <Select.Option value="demo">Demo</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="TreeSelect">
+            <TreeSelect
+              treeData={[
+                {
+                  title: "Light",
+                  value: "light",
+                  children: [
+                    {
+                      title: "Bamboo",
+                      value: "bamboo",
+                    },
+                  ],
+                },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item label="Cascader">
+            <Cascader
+              options={[
+                {
+                  value: "zhejiang",
+                  label: "Zhejiang",
+                  children: [
+                    {
+                      value: "hangzhou",
+                      label: "Hangzhou",
+                    },
+                  ],
+                },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item label="DatePicker">
+            <DatePicker />
+          </Form.Item>
+          <Form.Item label="RangePicker">
+            <RangePicker />
+          </Form.Item>
+          <Form.Item label="InputNumber">
+            <InputNumber />
+          </Form.Item>
+          <Form.Item label="TextArea">
+            <TextArea rows={4} />
+          </Form.Item>
+          <Form.Item label="Switch" valuePropName="checked">
+            <Switch />
+          </Form.Item>
+          <Form.Item
+            label="Upload"
+            valuePropName="fileList"
+            getValueFromEvent={normFile}
+          >
+            <Upload action="/upload.do" listType="picture-card">
+              <button
+                style={{
+                  border: 0,
+                  background: "none",
+                }}
+                type="button"
+              >
+                <PlusOutlined />
+                <div
+                  style={{
+                    marginTop: 8,
+                  }}
+                >
+                  Upload
+                </div>
+              </button>
+            </Upload>
+          </Form.Item>
+          <Form.Item label="Button">
+            <Button>Button</Button>
+          </Form.Item>
+          <Form.Item label="Slider">
+            <Slider />
+          </Form.Item>
+          <Form.Item label="ColorPicker">
+            <ColorPicker />
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
+export default FormDemo;
