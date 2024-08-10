be29fb2138edeed37e277676ef287062ce844b10
Rabeya Akter <rabeyaakterbdcit@gmail.com>
2024-08-10 03:33:54 -0700

    added modals

--- src/Components/Home/HomeContent/CardContent/CardContent.jsx
+++ src/Components/Home/HomeContent/CardContent/CardContent.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
 import {
   EditOutlined,
   EllipsisOutlined,
   SettingOutlined,
 } from "@ant-design/icons";
-import img from "../../../../assets/images/logo.png";
 import img2 from "../../../../assets/images/bg-works.jpg";
-import img3 from "../../../../assets/images/easy-customise.jpg";
 
-import { Avatar, Button, Card, ConfigProvider } from "antd";
+import { Avatar, Button, Card, ConfigProvider, Modal } from "antd";
 const { Meta } = Card;
 function CardContent() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className="bg-slate-500 pb-20">
+    <div className="bg-slate-200 pb-20">
       <div className="container mx-auto ">
-        <h1 className="text-4xl pt-10 text-center">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere,
-          excepturi!
+        <h1 className="text-4xl pt-20 text-center">
+          Lorem ipsum, dolor sit amet consectetur <br /> adipisicing elit.
+          Facere, excepturi!
         </h1>
         <p className="text-center pb-20">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
@@ -38,10 +48,8 @@ function CardContent() {
             },
             components: {
               Card: {
-                colorBgContainer: "rgb(212,175,135)",
-              },
-              Button: {
-                defaultBg: "rgb(231,140,42)",
+                colorBgContainer: "rgb(247,244,241)",
+                borderRadiusLG: 5,
               },
             },
           }}
@@ -59,35 +67,24 @@ function CardContent() {
                 title="Card title"
                 description="This is the description"
               />
-              <Button>Order</Button>
-            </Card>
-            <Card
-              className="text-center "
-              style={{
-                width: 300,
-              }}
-              cover={<img alt="example" className="h-72" src={img} />}
-            >
-              <Meta
-                className="text-center pb-8"
-                title="Card title"
-                description="This is the description"
-              />
-              <Button>Order</Button>
-            </Card>
-            <Card
-              className="text-center "
-              style={{
-                width: 300,
-              }}
-              cover={<img alt="example" className="h-72" src={img3} />}
-            >
-              <Meta
-                className="text-center pb-8"
-                title="Card title"
-                description="This is the description"
-              />
-              <Button>Order</Button>
+              <Button onClick={showModal}>Show More</Button>
+              <Modal
+                title="Product Details"
+                open={isModalOpen}
+                onOk={handleOk}
+                onCancel={handleCancel}
+              >
+                <div className="text-center">
+                  <h1>Lorem ipsum dolor sit amet.</h1>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Quo, doloremque.
+                  </p>
+                  <p>Some contents...</p>
+                  <p>Some contents...</p>
+                  <p>Some contents...</p>
+                </div>
+              </Modal>
             </Card>
           </div>
         </ConfigProvider>
